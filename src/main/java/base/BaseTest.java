ee26cc352f0a7a06699a453bd66fcec8e8b03c60
Abhijit Nagle <asnagle@gmail.com>
2025-06-11 16:58:39 +0530

    Integrated ExtendReports with capturing screenshots of failed testcases

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,14 +2,36 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 
+import utils.ExtentReportManager;
 import utils.log;
 
 public class BaseTest {
 	
 	protected WebDriver driver;
+	protected static ExtentReports extent;
+	protected ExtentTest test;
+	
+	@BeforeSuite
+	public void setupReport() {
+		
+		extent = ExtentReportManager.getReportInstance();
+		
+	}
+	
+	@AfterSuite
+	public void teardownReport() {
+		
+		extent.flush();
+	}
 	
 	@BeforeMethod
 	public void setUp() {
@@ -22,7 +44,14 @@ public class BaseTest {
 	}
 	
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) {
+		
+		if (result.getStatus() == ITestResult.FAILURE) {
+			
+			String screenshotPath = ExtentReportManager.captureScreenshot(driver, "LoginFailure");
+			test.fail("Test Failed.. Check Screenshot",
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		}
 		if (driver != null) {
 			log.info("Closing the Browser...");
 			driver.quit();
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExtentReportManager;
 import utils.log;
 
 public class LoginTest extends BaseTest {
@@ -11,31 +12,45 @@ public class LoginTest extends BaseTest {
 	@Test(priority=1)
 	public void validLogin() {
 		log.info("Starting to test Login with Valid Credentials...");
+		test = ExtentReportManager.createTest("Login with valid credentials Test");
+		
+		test.info("Navigating to URL");		
 		LoginPage loginPage = new LoginPage(driver);
+		
+		
 		log.info("Providing Credentials...");
+		test.info("Providing Credentials...");
 		loginPage.enterUsername("admin@yourstore.com");
 		loginPage.enterPassword("admin");
+		test.info("Clicking on Login Button");
 		loginPage.clickLogin();
 
 		System.out.println("Title is: " + driver.getTitle());
 		log.info("Verified Login Page...");
+		test.info("Verifying Page Title");
 		String pageTitle = driver.getTitle();
 		if (pageTitle.equalsIgnoreCase("Just a moment...")) {
 			Assert.assertEquals(driver.getTitle(), "Just a moment...");
 			log.info("Test completed Successfully ");
+			test.pass("Login Successful");
 		} else if (pageTitle.equalsIgnoreCase("Dashboard / nopCommerce administration")) {
 			Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration");
 			log.info("Test completed Successfully ");
+			test.pass("Login Successful");
 		}
-//		Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration");
 
 	}
 
 	@Test(priority=2)
 	public void invalidLogin() {
 		log.info("Starting to test Login with Invalid Credentials...");
+		test = ExtentReportManager.createTest("Login with Invalid credentials Test");
+		test.info("Navigating to URL");
+		
 		LoginPage loginPage = new LoginPage(driver);
+		
 		log.info("Providing Invalid Credentials...");
+		test.info("Providing Credentials...");
 		loginPage.enterUsername("admin@yourstore.com");
 		loginPage.enterPassword("admin123");
 		loginPage.clickLogin();
@@ -47,8 +62,9 @@ public class LoginTest extends BaseTest {
 				.trim().replaceAll("\\s+", " ");
 		System.out.println("Title is: " + driver.getTitle());
 		log.info("Printing Test result...");
+		test.info("Getting Login Page Error");
 		System.out.println("Failed Login error: "+actualErrorText);
 		Assert.assertEquals(actualErrorText, expectedErrorText, "Test Failed");
-
+		test.pass("Login Failed as expected");
 	}
 }
